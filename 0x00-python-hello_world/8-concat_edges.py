c736cd58118c2f8149d575dd57be3fef1bb6ce9a
Mowah Chibuike <mctimmykings@gmail.com>
2022-11-28 22:02:01 +0000

    tried another approach

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[str.find("object"): (str.find("object") + 28)] + "with Python"
+str = str[str.find("obj"): (str.find("obj") + 28)] + str[str.find("wi"): str.find("wi") + 5] + str[str.find("Py"): str.find("Py") + 6]
 print(str)
