b59974d501a1d071e6ccf250dc3cd8ecad460cce
Mowah Chibuike <mctimmykings@gmail.com>
2022-11-29 07:54:14 +0000

    removed string literals

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[str.find("obj"): (str.find("obj") + 28)] + str[str.find("wi"): str.find("wi") + 5] + str[str.find("Py"): str.find("Py") + 6]
+str = str[39:67] + str[107:112] + str[:6]
 print(str)
