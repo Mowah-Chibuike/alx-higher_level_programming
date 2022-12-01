f7459e0de6f03add89f724d35edd4f5747fbdfb1
Mowah Chibuike <mctimmykings@gmail.com>
2022-12-01 23:20:55 +0000

    handled pycodestyle errors

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,12 +3,18 @@
 import sys
 
 
-argv = sys.argv
-length = len(argv)
-if length > 2:
-    print("{:d} arguments:".format(length - 1))
-else:
-    print("{:d} argument.".format(length - 1))
+def print_args():
+    """print_args function: Prints arguments of the program"""
+    argv = sys.argv
+    length = len(argv)
+    if length > 2:
+        print("{:d} arguments:".format(length - 1))
+    else:
+        print("{:d} argument.".format(length - 1))
 
-for i in range(1, length):
-    print("{:d} : {}".format(i, argv[i]))
+    for i in range(1, length):
+        print("{:d} : {}".format(i, argv[i]))
+
+
+if __name__ == "__main__":
+    print_args()
