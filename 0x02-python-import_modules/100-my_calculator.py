812d7499c4cd5257853a9b8918c25a4c18db763d
Mowah Chibuike <mctimmykings@gmail.com>
2022-12-02 00:27:55 +0000

    added the 100-my_calculator.py script

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+import sys
+
+def calculator():
+    argv = sys.argv
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a = int(argv[1])
+    operator = argv[2]
+    b = int(argv[3])
+    if operator == "+":
+        print("{:d} {} {:d} = {:d}".format(a, operator, b, add(a, b)))
+    elif operator == "-":
+        print("{:d} {} {:d} = {:d}".format(a, operator, b, sub(a, b)))
+    elif operator == "*":
+        print("{:d} {} {:d} = {:d}".format(a, operator, b, mul(a, b)))
+    elif operator == "/":
+        print("{:d} {} {:d} = {:d}".format(a, operator, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    calculator()
--- 0x02-python-import_modules/README.md
+++ 0x02-python-import_modules/README.md
@@ -26,3 +26,6 @@ Tasks
 	- 3-infinite_add.py: program that prints the result of the addition of all arguments
 	- 4-hidden_discovery.py: program that prints all the names defined by the compiled module hidden_4.pyc
 	- 5-variable_load.py: program that imports the variable a from the file variable_load_5.py and prints its value.
+
+- Advanced
+	- 100-my_calculator.py: program that imports all functions from the file calculator_1.py and handles basic operations.
