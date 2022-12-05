17b0fc99ac8a8eef26770dcebd7e251426f7993d
Mowah Chibuike <mctimmykings@gmail.com>
2022-12-05 21:31:41 +0000

    handled pycodestyle errors

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
+    tuple1 = 
     if len(tuple_a) < 2:
         x = len(tuple_a) - 1
         for i in range(x, 3):
@@ -9,6 +10,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         x = len(tuple_a) - 1
         for i in range(x, 3):
             tuple_b += (0,) 
-    new_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] +tuple_b[1]
-    print("printer: {}".format(new_tuple))
+    new_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
     return new_tuple
