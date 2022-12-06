3f37bf47cf4a77eb9dab9999f49957be2a3bbe32
Mowah Chibuike <mctimmykings@gmail.com>
2022-12-06 22:49:22 +0000

    fixed bug

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,7 +6,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         for i in range(x, 3):
             tuple_a += (0,)
     if len(tuple_b) < 2:
-        x = len(tuple_a) - 1
+        x = len(tuple_b) - 1
         for i in range(x, 3):
             tuple_b += (0,) 
     new_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
