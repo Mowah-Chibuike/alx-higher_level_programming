47fbaff96ce0bccf4afcd2264497f175818682ea
Mowah Chibuike <mctimmykings@gmail.com>
2022-12-06 22:44:27 +0000

    checked if the list is set to None

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    new_list = my_list[:]
-    new_list.reverse()
-    for i in new_list:
-        print("{:d}".format(i))
+    if my_list != None:
+        new_list = my_list[:]
+        new_list.reverse()
+        for i in new_list:
+            print("{:d}".format(i))
