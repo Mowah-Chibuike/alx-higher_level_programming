2a1eb1611e97c23ec7dfe4abef3d39b186657073
Mowah Chibuike <mctimmykings@gmail.com>
2022-12-08 02:05:32 +0000

    added the 6-print_sorted_dictionary.py script

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    for x, y in dict(sorted(list(dictionary_unique.items()))).items():
+    for x, y in dict(sorted(list(a_dictionary.items()))).items():
         print("{}: {}".format(x, y))
