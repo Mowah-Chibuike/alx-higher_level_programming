b72262492ca6f500f1f18724fac9a930bd713513
Mowah Chibuike <mctimmykings@gmail.com>
2022-12-08 21:43:10 +0000

    tried a different approach

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
 def complex_delete(a_dictionary, value):
-    for x, y in a_dictionary.items():
-        if y == value:
-            a_dictionary.pop(x)
+    length = len(a_dictionary)
+    while length > 0:
+        for x, y in a_dictionary.items():
+            if y == value:
+                a_dictionary.pop(x)
+                break
+        length -= 1
     return dict(a_dictionary.items())
