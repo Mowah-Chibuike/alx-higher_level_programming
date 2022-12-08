60038973ef660d2ce20aa9faaa89c470cffc4f7f
Mowah Chibuike <mctimmykings@gmail.com>
2022-12-08 21:02:13 +0000

    handled pycodestyle errors

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,4 +3,6 @@
 def best_score(a_dictionary):
     if isinstance(a_dictionary, dict) and len(a_dictionary):
         res = max(list(a_dictionary.values()))
-        return list({x: y for x, y in a_dictionary.items() if y == res}.keys())[0]
+        for x, y in a_dictionary.items():
+            if y == res:
+                return x
