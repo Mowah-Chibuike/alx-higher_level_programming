6e681797b25c428039c9424a20e4a4a03e0d631e
Mowah Chibuike <mctimmykings@gmail.com>
2022-12-19 23:13:35 +0000

    fixed a bug

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     n = 0
     try:
         for i in range(x):
-            n += 1
             print(my_list[i], end="")
+            n += 1
         print()
     except Exception:
         print()
