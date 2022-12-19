dfe331e5821866826b1ae490d40117e187ae9881
Mowah Chibuike <mctimmykings@gmail.com>
2022-12-19 23:59:15 +0000

    added the 2-safe_print_list_integers.py script

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,13 +2,9 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     n = 0
-    #try:
     for i in range(x):
         if type(my_list[i]) is int:
             print(my_list[i], end="")
             n += 1
     print()
-    #except IndexError:
-       # raise IndexError("list index out of range")
-    #finally:
-    return n
+    return (n)
