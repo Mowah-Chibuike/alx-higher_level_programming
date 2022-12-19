22e191be0fb06e519e45180d73d5ff8f418eee61
Mowah Chibuike <mctimmykings@gmail.com>
2022-12-19 23:39:23 +0000

    handled pycodestyle errors

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
                 print(my_list[i], end="")
                 n += 1
         print()
-    except:
+    except Exception:
         print()
     finally:
         return n
