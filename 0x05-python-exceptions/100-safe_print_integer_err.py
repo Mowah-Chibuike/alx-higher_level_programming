2a88240a2befc2575ad3169ba3a5448f4b20b565
Mowah Chibuike <mctimmykings@gmail.com>
2022-12-20 01:46:04 +0000

    added the 100-safe_print_integer_err.py script

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except Exception as err:
-        sys.stderr.write("Exception: {}".format(str(err)))
+        sys.stderr.write("Exception: {}\n".format(str(err)))
         return False
