33bca88814e2185807e850f36a2a44e1631f987b
Mowah Chibuike <mctimmykings@gmail.com>
2022-12-20 00:05:55 +0000

    added the 2-safe_print_list_integers.py script

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,8 +4,8 @@ def safe_print_list_integers(my_list=[], x=0):
     n = 0
     try:
         for i in range(x):
-            if type(my_list[i]) is int:
-                print(my_list[i], end="")
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
                 n += 1
         print()
         return n
