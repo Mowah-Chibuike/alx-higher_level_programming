8301ee9a211aa751334e62187e4f37a38804e7ea
Mowah Chibuike <mctimmykings@gmail.com>
2022-12-21 02:45:50 +0000

    properly handled exceptions

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -76,7 +76,6 @@ class Square:
         """prints in stdout the square with the character #"""
         if self.__size == 0:
             print()
-            return
 
         for i in range(self.__position[1]):
             print()
