16305cf05c0a0eba1e44e12cc7fb70b4872ba749
Mowah Chibuike <mctimmykings@gmail.com>
2022-12-21 23:20:05 +0000

    handled pycodestyle errors

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -10,14 +10,13 @@ class MagicClass:
         - Public instance method: def circumference(self): that returns the
         circle perimeter
     """
-    def __init__(self, radius):
-        self.__radius = 0
+    def __init__(self, radius=0):
         if type(radius) is not float and type(radius) is not int:
             raise TypeError('radius must be a number')
         self.__radius = radius
-    
+
     def area(self):
-            return (self.__radius ** 2 * math.pi)
-    
+        return (self.__radius ** 2 * math.pi)
+
     def circumference(self):
-            return (2 * math.pi *self.__radius)
+        return (2 * math.pi * self.__radius)
