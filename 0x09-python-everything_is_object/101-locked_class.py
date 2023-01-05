ac4ec17c2251412896b1a3c2b4aa7dc58ca4d988
Mowah Chibuike <mctimmykings@gmail.com>
2023-01-05 04:17:39 +0000

    added the 101-locked_class.py script

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""Module contains class LockedClass"""
+
+
 class LockedClass:
-    __slot__ = ['first_name']
+    """LockedClass creates an object that prevents the user from dynamically
+    creating object attributes, except if the instance is called first_name
+    """
+    def __setattr__(self, name, value):
+        if name in self.__dict__ or name == 'first_name':
+            self.__dict__[name] = value
     pass
