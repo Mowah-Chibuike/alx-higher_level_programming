dfddecd4f0a74e073c21b6d2c97e00b65d2906c3
Mowah Chibuike <mctimmykings@gmail.com>
2023-01-09 15:20:52 +0000

    added the 2-is_same_class.py script

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,4 +5,7 @@ Module contains the function is_same_class
 
 
 def is_same_class(obj, a_class):
+    """
+    Returns True if obj is an instance of a_class, otherwise False
+    """
     return type(obj) is a_class
