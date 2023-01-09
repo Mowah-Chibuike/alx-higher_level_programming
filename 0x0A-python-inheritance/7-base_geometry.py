7dc874e9a142c86f9ece4d0d96d878fd16b37714
Mowah Chibuike <mctimmykings@gmail.com>
2023-01-09 17:37:04 +0000

    added the 7-base_geometry.py script an its test file

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module contains a BaseGeometry class
+"""
+
+
+class BaseGeometry(object):
+    """
+    BaseGeometry class
+    Public instance method: def area(self): that raises an exception with the\
+ message area() is not implemented
+    Public instance method: def integer_validator(self, name, value): that \
+validates value:
+        - if value is not an integer: raises a TypeError exception, with the \
+message <name> must be an integer
+        - if value is less or equal to 0: raise a ValueError exception with \
+the message <name> must be greater than 0
+    """
+    def area(self):
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """
+        Validates the value argument
+        """
+        if type(value) not in [int, float]:
+            raise TypeError('{:s} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{:s} must be greater than 0'.format(name))
