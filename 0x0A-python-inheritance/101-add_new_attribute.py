249b758d3a6e9e34dd58fc4ca14449369c953844
Mowah Chibuike <mctimmykings@gmail.com>
2023-01-09 23:09:26 +0000

    added the 101-add_new_attribute.py script

---
+++ 0x0A-python-inheritance/101-add_new_attribute.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module contains a function called add_attribute
+"""
+
+
+def add_attribute(obj, name, value):
+    """
+    Function adds a new attribute to a function if possible
+    Prototype: def add_attribute(obj, name, value)
+    Raises a TypeError exception if the object can't have the new attribute
+    """
+    if not hasattr(obj, '__setattr__'):
+        raise TypeError("can't add new attribute")
+
+    setattr(obj, name, value)
