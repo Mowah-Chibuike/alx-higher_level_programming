bfb2e77958e83a9ca814bcbc7eb5936d87587cee
Mowah Chibuike <mctimmykings@gmail.com>
2023-01-09 12:38:44 +0000

    added the 0-lookup.py script

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module contains a function called lookup
+"""
+
+
+def lookup(obj):
+    """
+    The function lookup prints  a list of all available attributes and \
+methods of an object
+    """
+    return dir(obj)
