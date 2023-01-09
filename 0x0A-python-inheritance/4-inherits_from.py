dce3b998039375cd8ecfe01f0448d9451bdb58ab
Mowah Chibuike <mctimmykings@gmail.com>
2023-01-09 16:45:35 +0000

    added the 4-inherits_from.py script

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module contains the function called inherits_from
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Prototype: def inherits_from(obj, a_class)
+    Returns True if obj is an instance of a class that inherited(directly or \
+indirectly) from a_class; otherwise False.
+    """
+    if obj is a_class:
+        return False
+    return issubclass(type(obj), a_class)
