3b23458f7a6ea6d2765b25e848714de34a5573e1
Mowah Chibuike <mctimmykings@gmail.com>
2023-01-10 23:10:05 +0000

    added the 4-from_json_string.py script

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -9,4 +9,4 @@ def from_json_string(my_str):
     """
     Returns an object(Python data structure) represented by a JSON string
     """
-    return json.dumps(my_str)
+    return json.loads(my_str)
