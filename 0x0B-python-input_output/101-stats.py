a23892fd61eb75ff43f8e77f57b4150edf2501ec
Mowah Chibuike <mctimmykings@gmail.com>
2023-01-11 19:55:05 +0000

    documented the module

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
-import sys
 """
 Module contains the Store class and the print_stats function
 """
+import sys
+
 
 class Store:
     """
     Stores state of statuses
     """
     def __init__(self):
+        """
+        Initializes an instance of teh Store class
+        """
         self.filesize = 0
         self.status = dict.fromkeys(
             ["200", "301", "400", "401", "403", "404", "405", "500"], 0)
 
     def update_store(self, filesize, status):
+        """
+        Updates the attributes of a store instance
+        """
         self.filesize += filesize
         self.status[status] += 1
 
     def print_store(self):
+        """
+        Prints the attributes of an instance of the Store class
+        """
         print("File size: {}".format(self.filesize))
         for status in sorted(self.status.keys()):
             if self.status[status] > 0:
