272278f698144a3de76891d072e08afcc51d84d7
Mowah Chibuike <mctimmykings@gmail.com>
2023-02-17 03:35:26 +0000

    added the 102-rating_shows.sql script

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,7 @@
+-- lists all shows from hbtn_0d_tvshows_rate by their ratings
+SELECT shows.title, SUM(ratings.rate)
+FROM tv_shows AS shows
+INNER JOIN tv_show_ratings AS ratings
+ON shows.id = ratings.show_id 
+GROUP BY shows.id
+ORDER BY SUM(ratings.rate) DESC;
