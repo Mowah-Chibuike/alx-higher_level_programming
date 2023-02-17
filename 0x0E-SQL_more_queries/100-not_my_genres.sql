4c68835f7e5b3c8a94a2a1a9068a4ba7bda1d665
Mowah Chibuike <mctimmykings@gmail.com>
2023-02-17 01:40:01 +0000

    added the 100-not_my_genres.sql script

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,17 @@
+-- uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+SELECT DISTINCT gen.name
+FROM tv_shows AS shows
+INNER JOIN tv_show_genres AS genres
+ON shows.id = genres.show_id
+INNER JOIN tv_genres AS gen
+ON genres.genre_id = gen.id
+WHERE gen.id NOT IN (
+	SELECT gen.id
+	FROM tv_shows as shows
+	INNER JOIN tv_show_genres AS genres
+	ON shows.id = genres.show_id
+	INNER JOIN tv_genres AS gen
+	ON genres.genre_id = gen.id
+	WHERE shows.title = "Dexter")
+ORDER BY gen.name;
