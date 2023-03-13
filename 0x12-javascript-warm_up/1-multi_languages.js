8a1118c33a2e03c7cfa7867d7072234f8bd464a4
Mowah Chibuike <mctimmykings@gmail.com>
2023-03-13 21:53:44 +0000

    fixed spelling error

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 let language;
-const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (language of languages) {
   console.log(language);
