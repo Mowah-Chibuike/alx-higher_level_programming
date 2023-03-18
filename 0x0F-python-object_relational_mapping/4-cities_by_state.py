454d8562f9fe785974e22bbbd52d73eca785f2c2
Mowah Chibuike <mctimmykings@gmail.com>
2023-03-18 23:19:44 +0000

    added the 4-cities_by_state.py script

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -2,15 +2,15 @@
 
 if __name__ == "__main__":
     from sys import argv
-    from MySQLdb import connect
+    import MySQLdb
 
     if (len(argv) == 4):
         username = argv[1]
         password = argv[2]
         dbname = argv[3]
 
-        db = connect(host="localhost", port=3306, user=username,
-                     password=password, database=dbname)
+        db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                             password=password, database=dbname)
         cursor = db.cursor()
         cursor.execute("""SELECT cities.id, cities.name, states.name\
                        FROM states\
