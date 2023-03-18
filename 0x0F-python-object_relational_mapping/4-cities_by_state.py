b197b2701e08f9606ae8363136c84ac33c28c12f
Mowah Chibuike <mctimmykings@gmail.com>
2023-03-18 23:16:58 +0000

    added the 4-cities_by_state.py script

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+    from MySQLdb import connect
+
+    if (len(argv) == 4):
+        username = argv[1]
+        password = argv[2]
+        dbname = argv[3]
+
+        db = connect(host="localhost", port=3306, user=username,
+                     password=password, database=dbname)
+        cursor = db.cursor()
+        cursor.execute("""SELECT cities.id, cities.name, states.name\
+                       FROM states\
+                       INNER JOIN cities ON states.id = cities.state_id\
+                       ORDER BY cities.id""")
+        res = cursor.fetchall()
+        for row in res:
+            print(row)
--- 0x0F-python-object_relational_mapping/README.md
+++ 0x0F-python-object_relational_mapping/README.md
@@ -23,3 +23,5 @@
 - 0-select_states.py: a script that lists all states from the database hbtn_0e_0_usa
 - 1-filter_states.py: a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
 - 2-my_filter_states.py: a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+- 3-my_safe_filter_states.py: a script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument but is safe from SQL injections
+- 4-cities_by_state.py: a script that lists all cities from the database hbtn_0e_4_usa
