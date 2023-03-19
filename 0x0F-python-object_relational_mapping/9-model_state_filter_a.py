6c1bd2352aa2c4220306e0ecf9fb0e5b44076e24
Mowah Chibuike <mctimmykings@gmail.com>
2023-03-19 19:46:28 +0000

    handled pycodestyle errors

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -18,6 +18,7 @@ if __name__ == "__main__":
         engine = create_engine("\
 mysql+mysqldb://{}:{}@localhost:3306/{}".format(username, password, dbname))
         session = Session(bind=engine)
-        states = session.query(State).filter(State.name.like("%a%")).order_by(State.id).all()
+        states = session.query(State).filter(
+                State.name.like("%a%")).order_by(State.id).all()
         for state in states:
             print("{}: {}".format(state.id, state.name))
