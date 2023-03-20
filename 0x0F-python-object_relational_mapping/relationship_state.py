7e1b6b32d5296d441f0f2daecaf7e10e25a471f2
Mowah Chibuike <mctimmykings@gmail.com>
2023-03-20 01:34:16 +0000

    added the 100-relationship_states_cities.py, relationship_city.py and relationship_state.py scripts

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+contains the class definition of a State and an instance Base = \
+declarative_base()
+"""
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import (Column, Integer, String)
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+links to the MySQL table state and is used by the ORM to map rows to \
+instances of State
+    """
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+    cities = relationship('City', back_populates='state',
+                          cascade='all, delete-orphan')
