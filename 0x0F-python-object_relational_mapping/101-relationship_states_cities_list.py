36b55f747687798d0abc288aad92fdfb2d933025
Mowah Chibuike <mctimmykings@gmail.com>
2023-03-20 02:22:02 +0000

    added the 101-relationship_states_cities_list.py script

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 lists all State objects, and corresponding City objects, contained in the
-database hbtn_0e_101_usa
+database hbtn_0e_101_usa using the SQLAlchemy module
 """
 
 if __name__ == "__main__":
