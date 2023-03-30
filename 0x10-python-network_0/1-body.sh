c98108362802b624f64c39de636f9b53133f2db0
Mowah Chibuike <mctimmykings@gmail.com>
2023-03-30 16:23:00 -0700

    added the 1-body.sh script

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s "$1"
+curl -s "$(curl -s --head -w "%{http_code}" "$1" | grep -q 200 && echo "$1")"
