c6ddb653db2009c6a473669dbcd1c2483792ad86
Mowah Chibuike <mctimmykings@gmail.com>
2023-03-31 11:18:05 -0700

    updated the 1-body.sh file

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s "$(curl -s --head "$1" | grep -q 200 && echo "$1")"
+curl -s "$(curl -sL --head "$1" | grep -q 200 && echo "$1")"
