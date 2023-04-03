2d7950f86241ff4cea3a436c93898fd071a9c8ba
Mowah Chibuike <mctimmykings@gmail.com>
2023-04-02 18:25:15 -0700

    added the 0-hbtn_status.py script

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,9 +9,4 @@ if __name__ == "__main__":
         content = body.read()
         print('\t- type: {}'.format(type(content)))
         print('\t- content: {}'.format(content))
-        print('\t- utf8 content:', end=" ")
-        try:
-            content.decode('utf-8')
-            print('OK')
-        except Exception:
-            print('NO')
+        print('\t- utf8 content: {}'.format(content.decode('utf-8')))
