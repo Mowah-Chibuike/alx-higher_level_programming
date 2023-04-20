477acffaf400b261704a35d2e995f5437bd338f5
Mowah-Chibuike <104087576+Mowah-Chibuike@users.noreply.github.com>
2023-04-20 18:01:25 +0100

    Delete 0x10-python-network_0 directory

--- 0x10-python-network_0/1-body.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -sL "$(curl -sL --head "$1" | grep -q 200 && echo "$1")"
--- 0x10-python-network_0/3-methods.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# displays all http methods a server will accept
-curl -sI "$1" | grep Allow | cut -d ' ' -f 2-
