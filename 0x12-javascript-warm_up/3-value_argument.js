c0b79ba5ff334105eeef575960e531f4f0f86486
Mowah-Chibuike <104087576+Mowah-Chibuike@users.noreply.github.com>
2023-04-20 18:02:52 +0100

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/3-value_argument.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const args0 = process.argv[2];
-
-if (args0) {
-  console.log(args0);
-} else {
-  console.log('No argument');
-}
--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-const args = process.argv;
-const arg1 = args[2];
-const arg2 = args[3];
-
-console.log(`${arg1} is ${arg2}`);
