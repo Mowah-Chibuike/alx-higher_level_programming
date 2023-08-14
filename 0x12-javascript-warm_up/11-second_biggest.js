659b4020a9c74a7172d461df49d1dd22041cace0
Mowah Chibuike <mctimmykings@gmail.com>
2023-08-14 21:25:51 +0000

    uploaded the 11-second_biggest.js script

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,6 @@ const argvLength = process.argv.length;
 if (argvLength <= 3) {
   console.log(0);
 } else {
-  const numbers = process.argv.map((ele) => Number(ele)).slice(2);
-  numbers.sort();
+  const numbers = process.argv.map((ele) => Number(ele)).slice(2).sort((a, b) => a - b);
   console.log(numbers[numbers.length - 2]);
 }
