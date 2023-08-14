4012348b010ba44e28ef416815f8f5f021bf4b0f
Mowah Chibuike <mctimmykings@gmail.com>
2023-08-14 20:10:12 +0000

    uploaded the 7-multi_c.js script

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const times = Number(process.argv[2]);
+if (times) {
+  for (let i = 0; i < times; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
