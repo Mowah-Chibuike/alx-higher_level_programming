590429cf41ee083aa20e9449dbe94355c524c22b
Mowah-Chibuike <104087576+Mowah-Chibuike@users.noreply.github.com>
2023-12-04 09:44:41 +0100

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-
-const list = require('./100-data').list;
-
-console.log(list);
-console.log(list.map((i, element) => element * i));
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-exports.esrever = function (list) {
-  const reversed = [];
-  list.forEach((element) => {
-    reversed.unshift(element);
-  });
-  return reversed;
-};
