ec0e9e530912be246ad7e4d9427544c16d8f0d01
Mowah Chibuike <mctimmykings@gmail.com>
2023-12-11 14:23:00 +0000

    upload the 9-add.js script

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// prints the addition of 2 integers
+
+function add (a, b) {
+  return (a + b);
+}
+
+const num1 = Number(process.argv[2]);
+const num2 = Number(process.argv[3]);
+console.log(add(num1, num2));
