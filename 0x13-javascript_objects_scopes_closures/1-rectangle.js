d64a24f7fcc5dac913bcb1135e1a5a1feb775b26
Mowah Chibuike <mctimmykings@gmail.com>
2023-12-12 21:04:45 +0000

    upload the 1-rectangle.js script

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,9 +2,6 @@
 // contains  a class Rectangle that defines a rectangle
 
 module.exports = class Rectangle {
-  width;
-  height;
-
   constructor (w, h) {
     this.width = w;
     this.height = h;
