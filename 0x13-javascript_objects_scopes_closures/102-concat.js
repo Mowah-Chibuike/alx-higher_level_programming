5f561252a3f1451ffbc2ce592d49b4554775b487
Mowah Chibuike <mctimmykings@gmail.com>
2023-12-12 23:42:10 +0100

    upload the 102-concat.js script

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+//  concats 2 files
+
+const fs = require('fs');
+const readA = process.argv[2];
+const readB = process.argv[3];
+const write = process.argv[4];
+
+fs.readFile(readA, (err, data) => {
+  if (err) {
+    throw err;
+  }
+  const text = data.toString();
+  fs.writeFile(write, text, () => {});
+});
+
+fs.readFile(readB, (err, data) => {
+  if (err) {
+    throw err;
+  }
+  const text = data.toString();
+  fs.appendFile(write, text, () => {});
+});
