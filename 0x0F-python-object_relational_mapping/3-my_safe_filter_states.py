96326f4e0040783474e8898fb8fe9ebea7854135
Mowah Chibuike <mctimmykings@gmail.com>
2023-12-17 18:46:06 +0000

    upload the 3-my_safe_filter_states.py script

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+takes in an argument and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument
+"""
+
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
+
+    if len(argv) > 3:
+        user = argv[1]
+        password = argv[2]
+        database = argv[3]
+        name_search = argv[4]
+
+        db = MySQLdb.connect(
+                host='localhost',
+                port=3306,
+                user=user,
+                passwd=password,
+                db=database
+        )
+
+        cur = db.cursor()
+        query = """SELECT * FROM states WHERE BINARY name = %s ORDER BY id"""
+        cur.execute(query, (name_search,))
+
+        results = cur.fetchall()
+        for state in results:
+            print(state)
+
+        cur.close()
+        db.close()
+    else:
+        print('Not enough arguments!!\n\
+Usage: script mysql_username mysql_password dbname')
