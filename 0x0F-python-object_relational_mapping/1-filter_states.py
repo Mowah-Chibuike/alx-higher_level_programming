769f2a04a282b29e9d359088310aa2f9ea41cb65
Mowah Chibuike <mctimmykings@gmail.com>
2023-12-17 18:08:03 +0000

    upload the 1-filter_states.py script

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+lists all states with a name starting with N (upper N) from a database
+"""
+
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
+
+    if len(argv) > 3:
+        user = argv[1]
+        password = argv[2]
+        database = argv[3]
+
+        db = MySQLdb.connect(
+                host='localhost',
+                port=3306,
+                user=user,
+                passwd=password,
+                db=database
+        )
+
+        cur = db.cursor()
+        cur.execute(
+                """
+SELECT * FROM states
+WHERE name LIKE "N%"
+ORDER BY id
+                """
+        )
+
+        results = cur.fetchall()
+        for state in results:
+            print(state)
+
+        cur.close()
+        db.close()
+    else:
+        print('Not enough arguments!!\n\
+Usage: script mysql_username mysql_password dbname')
