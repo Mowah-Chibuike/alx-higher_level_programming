80ee3eadd6aa9e705d764f38310003f63d72d956
Mowah Chibuike <mctimmykings@gmail.com>
2023-12-17 18:15:24 +0000

    fix error. Added the BINARY function to ensure it's upper N that is selected

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
         cur.execute(
                 """
 SELECT * FROM states
-WHERE name LIKE "N%"
+WHERE name LIKE BINARY "N%"
 ORDER BY id
                 """
         )
