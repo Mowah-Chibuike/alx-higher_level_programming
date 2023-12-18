9de11e5fde710fa3c990862c7ec5f2ceca9a05e8
Mowah Chibuike <mctimmykings@gmail.com>
2023-12-18 07:22:52 +0000

    upload the 12-model_state_update_id_2.py12-model_state_update_id_2.py script

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+changes the name of a State object from the database hbtn_0e_6_usa
+"""
+
+if __name__ == "__main__":
+    from sys import argv
+    from sqlalchemy import create_engine, URL
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+
+    if len(argv) > 3:
+        user = argv[1]
+        passwd = argv[2]
+        db = argv[3]
+
+        url = URL.create(
+                'mysql+mysqldb',
+                host='localhost',
+                port=3306,
+                username=user,
+                password=passwd,
+                database=db
+        )
+        engine = create_engine(url)
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(engine)
+
+        with Session() as session:
+            state = session.get(State, 2)
+            state.name = 'New Mexico'
+            session.commit()
