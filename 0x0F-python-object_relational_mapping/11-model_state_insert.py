f7c5cbcfe28b59dea292d14f2303683ac730e027
Mowah Chibuike <mctimmykings@gmail.com>
2023-12-18 07:02:40 +0000

    upload the 11-model_sttae_insert.py script

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+adds the State object “Louisiana” to the database hbtn_0e_6_usa
+"""
+
+if __name__ == "__main__":
+    from sys import argv
+    from sqlalchemy import create_engine, URL
+    from sqlalchemy.orm import Session
+    from model_state import Base, State
+
+    if len(argv) > 3:
+        username = argv[1]
+        password = argv[2]
+        database = argv[3]
+
+        url = URL.create(
+                'mysql+mysqldb',
+                host='localhost',
+                port=3306,
+                username=username,
+                password=password,
+                database=database
+        )
+
+        engine = create_engine(url)
+        Base.metadata.create_all(engine)
+        session = Session(bind=engine)
+
+        new_state = State(name='Louisiana')
+        session.add(new_state)
+        session.commit()
+
+        print(new_state.id)
